3b60030a1992b3f12dabee27c9952675005a6697
gifortunato2024 <168582782+gifortunato2024@users.noreply.github.com>
2024-06-05 15:14:35 -0300

    Update A2josir.py

--- A2josir.py
+++ A2josir.py
@@ -15,20 +15,20 @@ frequencia = st.selectbox("Com qual frequência você assiste?", ["Diariamente",
 canais_diferentes = st.radio("Você costuma assistir canais diferentes?", ("Sim", "Não, gosto de assistir o mesmo sempre "))
 
 if assiste_podcast == "Sim, amo!":
-st.header("Se sim, que bom! Vou te mostrar outros para você experimentar. Se você só assiste os mesmo, essa é uma ótima oportunidade para conhecer novos canais.")
-if st.button("Next"):  # Verifica se o botão "Next" foi pressionado
-    st.experimental_set_query_params(page=2)  # Altera para a página 2 após responder as perguntas
-    if st.experimental_get_query_params().get("page") == "2":
-        st.header("Qual nicho de podcast você gosta de assistir?")
-        st.header("Qual nicho de podcast você gosta de assistir?")
-        nicho = st.selectbox("Escolha um nicho", ["Conversas", "React", "Politica", "Empreendedorismo", "Paranormal", "Esporte", "Jogos", "Especialistas", "Tecnologia"])
-        nome_arquivo = "podcast_" + nicho.lower() + ".csv"
-        # Mostrar os 10 melhores canais de podcast no nicho escolhido
-        df = pd.read_csv(nome_arquivo)
-        st.header(f"Top 10 canais de podcast no YouTube brasileiro sobre {nicho}")
-        st.write(df)
-        # Adiciona a imagem no topo da primeira página
-        st.image("https://tecnoblog.net/noticias/youtube-teste-problema-desmonetizacao/", use_column_width=True)
+    st.header("Se sim, que bom! Vou te mostrar outros para você experimentar. Se você só assiste os mesmo, essa é uma ótima oportunidade para conhecer novos canais.")
+    if st.button("Next"):  # Verifica se o botão "Next" foi pressionado
+        st.experimental_set_query_params(page=2)  # Altera para a página 2 após responder as perguntas
+        if st.experimental_get_query_params().get("page") == "2":
+            st.header("Qual nicho de podcast você gosta de assistir?")
+            st.header("Qual nicho de podcast você gosta de assistir?")
+            nicho = st.selectbox("Escolha um nicho", ["Conversas", "React", "Politica", "Empreendedorismo", "Paranormal", "Esporte", "Jogos", "Especialistas", "Tecnologia"])
+            nome_arquivo = "podcast_" + nicho.lower() + ".csv"
+            # Mostrar os 10 melhores canais de podcast no nicho escolhido
+            df = pd.read_csv(nome_arquivo)
+            st.header(f"Top 10 canais de podcast no YouTube brasileiro sobre {nicho}")
+            st.write(df)
+            # Adiciona a imagem no topo da primeira página
+            st.image("https://tecnoblog.net/noticias/youtube-teste-problema-desmonetizacao/", use_column_width=True)
 else:
     st.write("Parece que você não assiste podcasts. Talvez você possa começar a assistir alguns dos nossos recomendados!")
 
