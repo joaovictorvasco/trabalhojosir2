697d9e8846225190786121ef3a4448acb93d0547
gifortunato2024 <168582782+gifortunato2024@users.noreply.github.com>
2024-06-05 10:45:35 -0300

    Update A2josir.py

--- A2josir.py
+++ A2josir.py
@@ -4,6 +4,9 @@ import pandas as pd
 st.sidebar.title ("Já conhece o nosso site?")
 st.sidebar.info ("Nosso site é para os amantes de podcast que estão em busca de novos canais. Aqui você pode escolher a categoria que você gosta e descobrir novas experiências. Aproveite!")
 
+foto = Image.open('camz.jpeg') 
+st.image(foto, caption="", width= 500)
+
 # Página 1: Perguntas sobre hábitos de assistir podcasts
 st.title("Amantes de Podcast")
 st.header("Vamos conhecer seus hábitos de assistir podcasts!")
