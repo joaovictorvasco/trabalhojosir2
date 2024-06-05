986a5b3da621688187b58f87d51b4fc377ec84d7
gifortunato2024 <168582782+gifortunato2024@users.noreply.github.com>
2024-06-05 14:33:16 -0300

    Update A2josir.py

--- A2josir.py
+++ A2josir.py
@@ -14,19 +14,17 @@ assiste_podcast = st.radio("Você costuma assistir podcast?", ("Sim, amo!", "Nã
 frequencia = st.selectbox("Com qual frequência você assiste?", ["Diariamente", "Semanalmente", "Mensalmente", "Raramente", "Nunca"])
 canais_diferentes = st.radio("Você costuma assistir canais diferentes?", ("Sim", "Não, gosto de assistir o mesmo sempre "))
 
-with st.form('form'):
-    if assiste_podcast == "Sim, amo!":
-        st.header("Se sim, que bom! Vou te mostrar outros para você experimentar. Se você só assiste os mesmo, essa é uma ótima oportunidade para conhecer novos canais. Vamos la? ")
-        
-        st.experimental_set_query_params(page=2)   # Altera para a página 2 após responder as perguntas
-    else:
-        st.write("Parece que você não assiste podcasts. Talvez você possa começar a assistir alguns dos nossos recomendados!")
+if assiste_podcast == "Sim, amo!":
+    st.header("Se sim, que bom! Vou te mostrar outros para você experimentar. Se você só assiste os mesmo, essa é uma ótima oportunidade para conhecer novos canais. Vamos la? ")
+    
+    st.experimental_set_query_params(page=2)   # Altera para a página 2 após responder as perguntas
+else:
+    st.write("Parece que você não assiste podcasts. Talvez você possa começar a assistir alguns dos nossos recomendados!")
     botao = st.from_submit_button('next') 
         
 
 # Página 2: Perguntar sobre o nicho de interesse
-#if "page" in st.experimental_get_query_params():
-if botao:
+if "page" in st.experimental_get_query_params():
     if st.experimental_get_query_params()["page"] == ["2"]:
         st.header("Qual nicho de podcast você gosta de assistir?")
         nicho = st.selectbox("Escolha um nicho", ["Conversas", "React", "Politica", "Empreendedorismo", "Paranormal", "Esporte", "Jogos", "Especialistas", "Tecnologia"])
